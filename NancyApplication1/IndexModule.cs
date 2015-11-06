00b580c703f201939d89ddefb03b43266a1ec4f7
Erik Hvattum <erik.hvattum@gmail.com>
2015-11-06 23:46:59 +0100

    initial commit, nancy app with only a module. an interface and a test

---
+++ NancyApplication1/IndexModule.cs
@@ -0,0 +1,21 @@
+﻿using Nancy;
+
+namespace NancyApplication1
+{
+    public class IndexModule : NancyModule
+    {
+        private readonly ITask task;
+
+
+        public IndexModule(ITask task)
+        {
+            this.task = task;
+            Get["/"] = parameters => task.GetValue();
+        }
+    }
+
+    public interface ITask
+    {
+        string GetValue();
+    }
+}
---
+++ NancyApplication1/InjectionTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace NancyApplication1
+{
+    [TestFixture]
+    public class InjectionTests
+    {
+        [Test]
+        public void Nsub_DoesNotGetCalled()
+        {
+            
+        }
+    }
+}
